4bb6a421955bf31e32e25e2f34506017c0ef0ffa
srivalli.kolla <kollasrivalli3@gmail.com>
2025-03-05 14:32:08 +0100

    conda activation added

--- cellbender/cellbender.sh
+++ cellbender/cellbender.sh
@@ -5,8 +5,8 @@ source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate cellbender
 
 # Define base directories
-base_input_dir="/home/gruengroup/srivalli/Github/Nuclear_hashing_2025/data"
-base_output_dir="/home/gruengroup/srivalli/Github/Nuclear_hashing_2025/data/cellbender_processed_data"
+base_input_dir="../Nuclear_hashing_2025/data"
+base_output_dir="../Nuclear_hashing_2025/data/cellbender_processed_data"
 
 # Define the FPR values
 fpr_values=(0.01 0.03 0.05 0.07 0.1)
@@ -29,7 +29,8 @@ run_cellbender_full() {
         --output "$output_dir/${fpr}_after_cb.h5" \
         --fpr "$fpr" \
         --model full \
-        --cpu-threads 48 
+        --epochs 50 \
+        --cuda
 }
 
 # Loop through FPR values and run CellBender
